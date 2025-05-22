95ada5233109313ba29ee41f7b528cf04af5f5cb
main <tompecher.official@gmail.com>
2025-05-22 11:35:48 +0100

    Rectangular grid

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-
-N = 50
-grid = np.random.choice([0, 1], size=(N, N), p=[0.8, 0.2])
+M, N = 150, 250
+grid = np.random.choice([0, 1], size=(M, N), p=[0.8, 0.2])
 
 def update(frame_num, img, grid):
     new_grid = grid.copy()
-    for i in range(N):
+    for i in range(M):
         for j in range(N):
             total = int((
                 grid[i, (j-1)%N] + grid[i, (j+1)%N] +
-                grid[(i-1)%N, j] + grid[(i+1)%N, j] +
-                grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] +
-                grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N]
+                grid[(i-1)%M, j] + grid[(i+1)%M, j] +
+                grid[(i-1)%M, (j-1)%N] + grid[(i-1)%M, (j+1)%N] +
+                grid[(i+1)%M, (j-1)%N] + grid[(i+1)%M, (j+1)%N]
             ))
+
             if grid[i, j] == 1:
                 if total < 2 or total > 3:
                     new_grid[i, j] = 0
@@ -27,11 +26,13 @@ def update(frame_num, img, grid):
     img.set_data(new_grid)
     grid[:] = new_grid[:]
     ax.set_title(f"Conway's Game of Life — Frame {frame_num}")
-    return img,
+    return img
 
 fig, ax = plt.subplots()
 img = ax.imshow(grid, interpolation='nearest', cmap='binary_r')
-ani = animation.FuncAnimation(fig,
+
+ani = animation.FuncAnimation(
+    fig,
     update,
     fargs=(img, grid),
     frames=1000,
@@ -40,4 +41,8 @@ ani = animation.FuncAnimation(fig,
 )
 
 plt.title("Conway's Game of Life")
+
+figManager = plt.get_current_fig_manager()
+figManager.full_screen_toggle()
+
 plt.show()
